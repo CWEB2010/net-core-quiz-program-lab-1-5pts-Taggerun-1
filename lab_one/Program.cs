25211fd47f9bd40f8111b2a6e2364fe8d462350f
Taggerun <chgmonroe1023@gmail.com>
2020-01-24 13:25:12 -0600

    Added Multidimensional Array and fixed errors.

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -18,22 +18,27 @@ namespace lab_one
             string userSelection;
             string[] questions = { "When was the latest version of .NET Core released?", "What makes .NET Core different from .NET Framework?",
             ""};
-            string[,] optionChoices =
-            {
-                {"A) December 31st, 2019", "B) December 3rd, 2019", "C) January 14th, 2020", "D) October 23rd, 2019"}
-
-            };
+            string[ , ] optionChoices = { 
+                                {"A) December 31st, 2019", "B) December 3rd, 2019", "C) January 14th, 2020", "D) October 23rd, 2019"}, 
+                                {"A) ","", "", ""} 
+                                };
 
             int x;
+            int y;
 
             Console.WriteLine("Welcome to this quiz on .NET Core." + "\n" +
                 "This quiz contains 10 questions." + "\n" +
                 "You must obtain a 7 out of 10 to pass this quiz.");
+            Console.WriteLine();
             Console.Clear();
 
             for (x = 0; x < questions.Length; x++)
             {
                 Console.WriteLine(questions[x]);
+                for (y = 0; y < optionChoices.GetLength(1); y++)
+                {
+                    Console.WriteLine(optionChoices[x, y]);
+                }
             }
         }
     }
